03b073a324d82e910afd0afd3ce8f5db0057a6d4
“Marijus84” <marijus.romanovas@hotmail.com>
2023-01-24 11:46:38 +0200

    first commit

---
+++ crud.js
@@ -0,0 +1,13 @@
+const BASE_URL = "https://kaker-js-crud.onrender.com/cafe-04/party";
+
+//! GET
+
+//! POST
+//after POST run line below:
+// window.location.reload();
+
+//! PUT
+// window.location.reload();
+
+//! DELETE
+// window.location.reload();
